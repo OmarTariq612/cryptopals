d38376f0b88c6bc8d410fb0351f3cb6f0d80156b
OmarTariq612 <az2339913@gmail.com>
2023-07-01 10:05:32 +0300

    set 1 - challenge 5

---
+++ set1/challenge5.go
@@ -0,0 +1,29 @@
+package set1
+
+import (
+	"errors"
+	"strings"
+)
+
+func EncryptWithRepeatingXORKey(input, key string) (string, error) {
+	if len(key) == 0 {
+		return "", errors.New("key length must be at least one")
+	}
+
+	inputLength := len(input)
+	keyLength := len(key)
+
+	bytes := make([]byte, inputLength)
+	for i := 0; i < inputLength; i++ {
+		bytes[i] = input[i] ^ key[i%keyLength]
+	}
+
+	out := &strings.Builder{}
+	out.Grow(inputLength / 2)
+	for i := 0; i < inputLength; i++ {
+		out.WriteByte(base16space[bytes[i]>>4])
+		out.WriteByte(base16space[bytes[i]&0b00001111])
+	}
+
+	return out.String(), nil
+}
---
+++ set1/challenge5_test.go
@@ -0,0 +1,20 @@
+package set1
+
+import "testing"
+
+func TestEncryptWithRepeatingXORKey(t *testing.T) {
+
+	input := `Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal`
+
+	expectedOutput := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+	actualOutput, err := EncryptWithRepeatingXORKey(input, "ICE")
+	if err != nil {
+		t.Errorf("err: %s", err.Error())
+	}
+
+	if expectedOutput != actualOutput {
+		t.Errorf("%s\nAND\n%s\nARE NOT THE SAME!\n", actualOutput, expectedOutput)
+	}
+}
